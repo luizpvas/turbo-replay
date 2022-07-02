7d602e04d61bd1edc71ded81de9c69d9181f96e0
Luiz Paulo <luiz.pv9@gmail.com>
2022-07-02 11:27:34 -0300

    make sure treamschannel is loaded before monkey patch

--- lib/turbo/replay/engine.rb
+++ lib/turbo/replay/engine.rb
@@ -5,6 +5,8 @@ module Turbo::Replay
     isolate_namespace Turbo::Replay
 
     initializer "turbo-replay.overrides" do
+      Turbo::StreamsChannel.class # eager load
+
       class Turbo::StreamsChannel
         extend Overrides::StreamsChannelBroadcast
         prepend Overrides::StreamsChannelReceived
