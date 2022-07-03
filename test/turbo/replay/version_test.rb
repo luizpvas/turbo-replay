6658da228a582f8a1a119198c698eb366cac6159
Luiz Paulo <luiz.pv9@gmail.com>
2022-07-03 10:27:15 -0300

    bump version

--- test/turbo/replay/version_test.rb
+++ test/turbo/replay/version_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class Turbo::Replay::VersionTest < ActiveSupport::TestCase
   test "it has a version number" do
-    assert_equal("0.1.1", Turbo::Replay::VERSION)
+    assert_equal("0.1.2", Turbo::Replay::VERSION)
   end
 end
